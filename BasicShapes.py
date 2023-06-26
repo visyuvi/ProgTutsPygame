4e0e22eb4511b98e0b94a8db5a301edfb4b1bc30
Your Name <you@example.com>
2023-06-26 10:22:49 +0530

    Code to display rectangle at origin. Its color changes to either blue or red when down arrow is pressed

--- BasicShapes.py
+++ BasicShapes.py
@@ -11,7 +11,7 @@ def setup():
 
 def main():
     done = False
-
+    isBlue = True
     while not done:
         screen.fill((0, 0, 0))
         # go through the event queue
@@ -19,6 +19,14 @@ def main():
             if e.type == p.QUIT:
                 done = True
 
+            if e.type == p.KEYDOWN and e.key == p.K_DOWN:
+                isBlue = not isBlue
+        if isBlue:
+            color = (56, 82, 186)  # 0 - 255
+        else:
+            color = (160, 50, 50)
+        rectangle1 = p.Rect(0, 0, 50, 50)
+        p.draw.rect(screen, color, rectangle1)  # x, y, width, height
         p.display.flip()  # redraws the display
         clock.tick(30)  # max out af 30 fps
 
