36670fc06e6ab07dc31a9990f8a98692518ebfd0
Your Name <you@example.com>
2023-06-26 10:40:25 +0530

    Code to draw multiple rectangles and circles starting at 300,300

--- BasicShapes.py
+++ BasicShapes.py
@@ -12,6 +12,12 @@ def setup():
 def main():
     done = False
     isBlue = True
+
+    x = 300
+    y = 300
+    w = 50
+    h = 50
+
     while not done:
         screen.fill((0, 0, 0))
         # go through the event queue
@@ -25,8 +31,11 @@ def main():
             color = (56, 82, 186)  # 0 - 255
         else:
             color = (160, 50, 50)
-        rectangle1 = p.Rect(0, 0, 50, 50)
-        p.draw.rect(screen, color, rectangle1)  # x, y, width, height
+
+        for i in range(10):
+            rectangle1 = p.Rect(x + 25 * i, y + 25 * i, w, h)  # x, y, width, height
+            p.draw.rect(screen, color, rectangle1, width=10)
+            p.draw.circle(screen, color, (x + 25 * i, y + 25 * i), 25, 10)
         p.display.flip()  # redraws the display
         clock.tick(30)  # max out af 30 fps
 
