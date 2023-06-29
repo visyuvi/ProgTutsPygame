81a176bfe5ccc2f111318d437f6743dd290acece
Your Name <you@example.com>
2023-06-29 16:00:31 +0530

    Code to jump the rectangle using space bar key

--- TechWithTimPygame/firstGame.py
+++ TechWithTimPygame/firstGame.py
@@ -6,13 +6,14 @@ screenHeight = 500
 win = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("First Game")
 
-
-
 x = 50
-y = 50
+y = 430
 width = 40
 height = 60
 vel = 5
+
+isJump = False
+jumpCount = 10
 
 run = True
 while run:
@@ -28,10 +29,24 @@ while run:
         x -= vel
     if keys[pygame.K_RIGHT] and x + width + vel < screenWidth:
         x += vel
-    if keys[pygame.K_UP] and y > vel:
-        y -= vel
-    if keys[pygame.K_DOWN] and y + height + vel < screenHeight:
-        y += vel
+    if not isJump:
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+        if keys[pygame.K_DOWN] and y + height + vel < screenHeight:
+            y += vel
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
+
+    if keys[pygame.K_SPACE]:
+        isJump = True
 
     win.fill((0, 0, 0))
     pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
